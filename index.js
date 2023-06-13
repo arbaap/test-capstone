a631cdffab8ef14996397a58a6b236139578e073
arbaap <arbaadhipamungkas@gmail.com>
2023-06-13 17:35:29 +0700

    as

--- index.js
+++ index.js
@@ -148,6 +148,22 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+app.get("/users/me", async (req, res) => {
+  const userId = req.user._id;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: true, message: "User not found" });
+    }
+
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ error: true, message: "Server error" });
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
